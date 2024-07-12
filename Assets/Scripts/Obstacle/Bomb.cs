25ea67ed96dc84266a278ef4ceca8938189cf2fa
이보현(Lee bohyeon) <42289387+leebohyeon1@users.noreply.github.com>
2024-07-13 02:14:44 +0900

    add asset

--- Assets/Scripts/Obstacle/Bomb.cs
+++ Assets/Scripts/Obstacle/Bomb.cs
@@ -6,11 +6,8 @@ public class Bomb : MonoBehaviour
 {
     public GameObject explosion;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //transform.DOScale(new Vector3(transform.localScale.x * 5, transform.localScale.y * 5,
-        //    transform.localScale.z * 5), 2f).SetEase(Ease.InQuad).OnComplete(()=> Attack());
     }
 
     void Attack()
